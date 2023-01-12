b7965edb30d8d420e952195e9f2aa949bf89475d
Ilya Griss <ilyaig8@gmail.com>
2023-01-12 22:55:41 +0200

    mq

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -3,57 +3,47 @@ import YOUTUSE from '../../src/assets/images/youtuse.png'
 import IGAMES from '../../src/assets/images/cover.jpg'
 
 const Portfolio = () => {
+  const data = [
+    {
+      id: 1,
+      image: YOUTUSE,
+      title: 'This is a youtube clone!',
+      github: 'https://github.com/ilyaig87/yotube_clone',
+      demo: 'https://youtuse.netlify.app/',
+    },
+    {
+      id: 2,
+      image: IGAMES,
+      title: 'This is a game site!',
+      github: 'https://github.com/ilyaig87/Memory-Game',
+      demo: 'https://i-games.netlify.app/',
+    },
+  ]
+
   return (
     <section id='portfolio'>
       <h5>My Recent Projects</h5>
       <h2>Portfolio</h2>
 
       <div className='container portfolio-container grid'>
-        <article className='portfolio-item'>
-          <div className='portfolio-item-image'>
-            <img src={YOUTUSE} alt='YOUTUSE' className='portfolio-img' />
-            <h3>This is a youtube clone! </h3>
-            <div className='buttons-portfolio flex'>
-              <a
-                href='https://github.com/ilyaig87/yotube_clone'
-                target='_blank'
-                className='btn'
-              >
-                Github
-              </a>
-              <a
-                href='https://youtuse.netlify.app/'
-                target='_blank'
-                className='btn btn-primary'
-              >
-                Check it here!
-              </a>
-            </div>
-          </div>
-        </article>
-
-        <article className='portfolio-item'>
-          <div className='portfolio-item-image'>
-            <img src={IGAMES} alt='IGAMES' className='portfolio-img' />
-            <h3>This is a game site! </h3>
-            <div className='buttons-portfolio flex'>
-              <a
-                href='https://github.com/ilyaig87/Memory-Game'
-                target='_blank'
-                className='btn'
-              >
-                Github
-              </a>
-              <a
-                href='https://i-games.netlify.app/'
-                target='_blank'
-                className='btn btn-primary'
-              >
-                Check it here!
-              </a>
-            </div>
-          </div>
-        </article>
+        {data.map(({ id, image, title, github, demo }) => {
+          return (
+            <article className='portfolio-item' key={id}>
+              <div className='portfolio-item-image'>
+                <img src={image} alt={image} className='portfolio-img' />
+                <h3>{title}</h3>
+                <div className='buttons-portfolio flex'>
+                  <a href={github} target='_blank' className='btn'>
+                    Github
+                  </a>
+                  <a href={demo} target='_blank' className='btn btn-primary'>
+                    Check it here!
+                  </a>
+                </div>
+              </div>
+            </article>
+          )
+        })}
       </div>
     </section>
   )
