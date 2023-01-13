621a531981c1bc30fe46e166109129708f461b7e
Ilya Griss <ilyaig8@gmail.com>
2023-01-13 11:32:25 +0200

    education 2

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
+import '../assets/styles/education.css'
+import { TbSchool } from 'react-icons/tb'
 
 const Education = () => {
   return (
     <section id='education'>
       <h5></h5>
       <h2>My Education</h2>
+
+      <div className='container education-container '>
+        <article className='education grid'>
+          <section className='education-section'>
+            <TbSchool />
+            <h3>CODING ACADEMY - FullStack Web Developer</h3>
+            <small>July 2O22 - November 2O22</small>
+            <p>
+              4 months full time course (8:30 AM - 18:30 PM), covering a wide
+              variety of programming topics such as data structures, networking,
+              OOP as well as hands on experience with projects in
+              Javascript,HTML, CSS React JS, Vue and Node.
+            </p>
+          </section>
+          <section className='education-section'>
+            <TbSchool />
+            <h3>CODING ACADEMY - FullStack Web Developer</h3>
+            <small>July 2O22 - November 2O22</small>
+            <p>
+              4 months full time course (8:30 AM - 18:30 PM), covering a wide
+              variety of programming topics such as data structures, networking,
+              OOP as well as hands on experience with projects in
+              Javascript,HTML, CSS React JS, Vue and Node.
+            </p>
+          </section>
+          <section className='education-section'>
+            <TbSchool />
+            <h3>CODING ACADEMY - FullStack Web Developer</h3>
+            <small>July 2O22 - November 2O22</small>
+            <p>
+              4 months full time course (8:30 AM - 18:30 PM), covering a wide
+              variety of programming topics such as data structures, networking,
+              OOP as well as hands on experience with projects in
+              Javascript,HTML, CSS React JS, Vue and Node.
+            </p>
+          </section>
+        </article>
+      </div>
     </section>
   )
 }
