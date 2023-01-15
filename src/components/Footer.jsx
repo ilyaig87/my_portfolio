724487d6eea31e71fc98f9c26eb32febae124c87
Ilya Griss <ilyaig8@gmail.com>
2023-01-15 19:03:44 +0200

    footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,56 @@
 import '../assets/styles/footer.css'
+import { FaFacebook } from 'react-icons/fa'
+import { BsLinkedin } from 'react-icons/bs'
+import { AiFillGithub } from 'react-icons/ai'
 
 const Footer = () => {
-  return <div className='footer-container'>Footer</div>
+  return (
+    <footer>
+      <a href='#' className='footer-logo'>
+        Ilya Griss
+      </a>
+
+      <ul className='footer-links flex'>
+        <li>
+          <a href='#'>Home</a>
+        </li>
+        <li>
+          <a href='#about'>About</a>
+        </li>
+        <li>
+          <a href='#experience'>Experience</a>
+        </li>
+        <li>
+          <a href='#portfolio'>Portfolio</a>
+        </li>
+        <li>
+          <a href='#jobExperience'>Job Experience</a>
+        </li>
+        <li>
+          <a href='#education'>Education</a>
+        </li>
+        <li>
+          <a href='#contact'>Contact</a>
+        </li>
+      </ul>
+
+      <div className='footer-socials flex'>
+        <a href='https://.facebook.com'>
+          <FaFacebook />
+        </a>
+        <a href='https://github.com'>
+          <BsLinkedin />
+        </a>
+        <a href='https://linkdin.com'>
+          <AiFillGithub />
+        </a>
+      </div>
+
+      <div className='footer-copyright'>
+        <small>&copy; Ilya Griss. All rights reserved.</small>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
